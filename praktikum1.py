53ee193edfd6cf902b5411130a4b7fc7b0ae40c8
rais <raismuf@gmail.com>
2019-02-03 13:40:56 +0700

    mencari nilai terbesar dari 3 buah bilangan yang diinputkan

---
+++ praktikum1.py
@@ -0,0 +1,15 @@
+a=int (input ('bilangan a : '))
+b=int (input ('bilangan b : '))
+c=int (input ('bilangan c : '))
+
+if a>b :
+	if a>c :
+		print(a)
+	else :
+		print (c)
+elif b>c :
+	print(b)
+else :
+	print (c)
+
+
